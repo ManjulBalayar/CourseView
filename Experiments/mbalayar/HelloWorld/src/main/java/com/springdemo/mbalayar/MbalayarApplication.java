4537228b8d344ea2a4608b6d6b7e08ac9a1a3e3f
Manjul Balayar <manjulbalayar@manjuls-air.misc.iastate.edu>
2023-09-07 00:06:14 -0500

    Simple Hello World

---
+++ Experiments/mbalayar/HelloWorld/src/main/java/com/springdemo/mbalayar/MbalayarApplication.java
@@ -0,0 +1,14 @@
+package com.springdemo.mbalayar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MbalayarApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(MbalayarApplication.class, args);
+    }
+
+}
