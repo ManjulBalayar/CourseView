0018e7bf9749ae15c0b44a3d8fa0de6aea4518fb
BenjaminMuslic <muslic.ben@gmail.com>
2023-09-06 23:03:30 -0500

    added Devin to app!

---
+++ Experiments/muslicb/exp2/AndroidVolley/app/src/main/java/com/example/sumon/androidvolley/utils/Const.java
@@ -0,0 +1,8 @@
+package com.example.sumon.androidvolley.utils;
+
+public class Const {
+	public static final String URL_JSON_OBJECT = "https://jsonplaceholder.typicode.com/users/1";
+	public static final String URL_JSON_ARRAY = "https://jsonplaceholder.typicode.com/users";
+	public static final String URL_STRING_REQ = "https://jsonplaceholder.typicode.com/users/1";
+	public static final String URL_IMAGE = "https://media.licdn.com/dms/image/D5603AQHDgE0IrnsFqg/profile-displayphoto-shrink_100_100/0/1686689847302?e=1699488000&v=beta&t=LcmV3CiwOI30DfbAvw2kcsfoelV2uUtwIu5k_AhGXKY";
+}
