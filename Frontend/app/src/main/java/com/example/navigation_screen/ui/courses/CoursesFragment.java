653425d9d6b3629631d4f7d06885921ce11456f6
BenjaminMuslic <muslic.ben@gmail.com>
2023-09-27 14:46:19 -0500

    Commiting just in case I break anything and need to revert back.

---
+++ Frontend/app/src/main/java/com/example/navigation_screen/ui/courses/CoursesFragment.java
@@ -0,0 +1,37 @@
+package com.example.navigation_screen.ui.courses;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.example.navigation_screen.databinding.FragmentHomeBinding;
+
+public class CoursesFragment extends Fragment {
+
+    private FragmentHomeBinding binding;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        CourseViewModel courseViewModel =
+                new ViewModelProvider(this).get(CourseViewModel.class);
+
+        binding = FragmentHomeBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+        final TextView textView = binding.textHome;
+        courseViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        return root;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}
