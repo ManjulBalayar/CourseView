243abfb5345ef5e7786c43768f86bea05d09b47c
Manjul Balayar <manjulbalayar@manjuls-air.misc.iastate.edu>
2023-10-01 20:36:13 -0500

    Switched branches

---
+++ Experiments/mbalayar/Demo2/src/main/java/coms309/roundtrip/demo2/Model/Advisors.java
@@ -0,0 +1,8 @@
+//package coms309.roundtrip.demo2.Model;
+//
+//public class Advisors extends Users{
+//
+//    public Advisors(int id, String name, String email) {
+//        super(id, name, email);
+//    }
+//}
