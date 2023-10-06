acd3cf6ae504decdff42321dd345291803212732
dalamsya50 <devin.alamsya@gmail.com>
2023-10-06 15:13:01 -0500

    Getting the switching between pages working (login/signup) part 3

--- Frontend/app/src/main/java/com/example/navigation_screen/Login.java
+++ Frontend/app/src/main/java/com/example/navigation_screen/Login.java
@@ -1,8 +1,16 @@
 package com.example.navigation_screen;
 
 import android.os.Bundle;
-import android.widget.EditText;
+import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+
+import com.example.navigation_screen.databinding.ActivityMainBinding;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -14,6 +22,8 @@ public class Login extends AppCompatActivity{
     EditText username, password;
     Button loginbtn;
 
+    private ActivityMainBinding binding;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,6 +33,24 @@ public class Login extends AppCompatActivity{
         password = findViewById(R.id.password);
         loginbtn = findViewById(R.id.loginbtn);
 
+        loginbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                binding = ActivityMainBinding.inflate(getLayoutInflater());
+                setContentView(binding.getRoot());
+
+                    BottomNavigationView navView = findViewById(R.id.nav_view);
+                    // Passing each menu ID as a set of Ids because each
+                    // menu should be considered as top level destinations.
+                     AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+                        R.id.courses, R.id.chat, R.id.profile, R.id.schedule)
+                        .build();
+                    NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
+                    NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+                    NavigationUI.setupWithNavController(binding.navView, navController);
+
+            }
+        });
 
 
 
--- Frontend/app/src/main/java/com/example/navigation_screen/SignUp.java
+++ Frontend/app/src/main/java/com/example/navigation_screen/SignUp.java
@@ -7,6 +7,7 @@ import android.widget.Spinner;
 import androidx.appcompat.app.AppCompatActivity;
 
 
+
 public class SignUp extends AppCompatActivity{
 
     @Override
