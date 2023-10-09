991be53870611a38b2c960eb0ad27f34126d9683
Manjul Balayar <mbalayar@iastate.edu>
2023-10-09 14:24:40 -0500

    Finishing my mango backend

--- Backend/app/src/main/java/com/example/app/controller/LoginController.java
+++ Backend/app/src/main/java/com/example/app/controller/LoginController.java
@@ -5,6 +5,7 @@ import com.example.app.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Map;
 
 @RestController
@@ -14,6 +15,11 @@ public class LoginController {
     @Autowired
     UserRepository UserRepo;
 
+    @GetMapping("/users/{username}")
+    public List<Users> getStudent(@PathVariable("username") String username) {
+        return UserRepo.findAllByUsername(username);
+    }
+
     @PostMapping("/login")
     public Users getUser(@RequestBody Map<String, Object> requestBody) {
 
@@ -21,15 +27,14 @@ public class LoginController {
         String password = requestBody.get("password").toString();
 
         Users user = UserRepo.findByUsername(userName);
-
-
-
+        // check if user exists / password is correct
         if (user != null && user.getPassword().equals(password)) {
             return user;
         }
         else {
             return null;
         }
+
 
     }
 
--- Backend/app/src/main/java/com/example/app/repository/UserRepository.java
+++ Backend/app/src/main/java/com/example/app/repository/UserRepository.java
@@ -3,6 +3,11 @@ package com.example.app.repository;
 import com.example.app.model.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface UserRepository extends JpaRepository<Users, Long> {
     Users findByUsername(String username);
+
+    // This will return a list of Users with the specified username
+    List<Users> findAllByUsername(String username);
 }
