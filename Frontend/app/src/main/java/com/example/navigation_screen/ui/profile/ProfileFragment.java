009b2a76198100acc7c1468eb4c8d265b0a6de5f
dalamsya50 <devin.alamsya@gmail.com>
2023-10-20 10:44:10 -0500

    Get method skeleton for profile page

--- Frontend/app/src/main/java/com/example/navigation_screen/ui/profile/ProfileFragment.java
+++ Frontend/app/src/main/java/com/example/navigation_screen/ui/profile/ProfileFragment.java
@@ -1,6 +1,7 @@
 package com.example.navigation_screen.ui.profile;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,7 +12,16 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
 
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
 import com.example.navigation_screen.databinding.FragmentProfileBinding;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class ProfileFragment extends Fragment {
 
@@ -37,4 +47,37 @@ public class ProfileFragment extends Fragment {
         super.onDestroyView();
         binding = null;
     }
+
+    // This method loads courses from a specified URL and populates course-related variables.
+    private void loadCourses() {
+        // URL pointing to the courses resource
+        String url = "http://coms-309-030.class.las.iastate.edu:8080/courses";
+
+        // Create a new JSON array request to receive a JSON array from the given URL
+        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(Request.Method.GET, url, null,
+                // Listener for successful responses
+                new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        try {
+
+                        } catch (JSONException e) {
+                            // Print stack trace for JSONException
+                            e.printStackTrace();
+                        }
+                    }
+                },
+                // Listener for error responses
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        // Log error responses with tag "VolleyError"
+                        Log.e("VolleyError", error.toString());
+                    }
+                });
+
+        // Add the created request to the Volley request queue
+        Volley.newRequestQueue(getContext()).add(jsonArrayRequest);
+    }
+
 }
