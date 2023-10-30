26b16125b7c62f4963321b781e5c8cf0bafe8f01
Kalem S <kalemschrock@gmail.com>
2023-10-30 15:28:55 -0500

    basic version of Course class

---
+++ Backend/app/src/main/java/com/example/app/controller/CourseController.java
@@ -0,0 +1,43 @@
+package com.example.app.controller;
+
+import com.example.app.model.Course;
+import com.example.app.repository.CourseRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+@RestController
+public class CourseController {
+
+    @Autowired
+    CourseRepository repo;
+
+    // POST METHODS
+    @PostMapping("/course")
+    public Course addAlien(@RequestBody Course course) {
+        repo.save(course);
+        return course;
+    }
+    // END of POST METHODS
+
+
+    // GET METHODS
+
+    // Get certain course
+    @GetMapping("/course/{course_id}")
+    public Optional<Course> getStudent(@PathVariable("course_id") Long course_id) {
+        return repo.findById(course_id);
+    }
+
+    @GetMapping("/courses")
+    public List<Course> getAliens() {
+
+        return repo.findAll();
+    }
+
+
+    // END of GET METHODS
+
+
+}
--- Backend/app/src/main/java/com/example/app/model/Course.java
+++ Backend/app/src/main/java/com/example/app/model/Course.java
@@ -1,4 +1,57 @@
 package com.example.app.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Course {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long course_id;
+
+    private String course_name;
+
+    private String course_description;
+
+    private String course_department;
+
+    // TODO
+    // 1) add relationship oneToMany with review
+    // 2) add relationship oneToMany with shcedule
+
+
+    public Long getCourse_id() {
+        return course_id;
+    }
+
+    public void setCourse_id(Long course_id) {
+        this.course_id = course_id;
+    }
+
+    public String getCourse_name() {
+        return course_name;
+    }
+
+    public void setCourse_name(String course_name) {
+        this.course_name = course_name;
+    }
+
+    public String getCourse_description() {
+        return course_description;
+    }
+
+    public void setCourse_description(String course_description) {
+        this.course_description = course_description;
+    }
+
+    public String getCourse_department() {
+        return course_department;
+    }
+
+    public void setCourse_department(String course_department) {
+        this.course_department = course_department;
+    }
 }
