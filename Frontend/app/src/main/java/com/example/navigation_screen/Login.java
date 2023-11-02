51bd019687edca1ee0c2e9f382dffe18cc76bc2a
dalamsya50 <devin.alamsya@gmail.com>
2023-11-02 10:34:36 -0500

    Update some code to stop bugs

--- Frontend/app/src/main/java/com/example/navigation_screen/Login.java
+++ Frontend/app/src/main/java/com/example/navigation_screen/Login.java
@@ -5,7 +5,6 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
@@ -17,12 +16,13 @@ import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
-import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
 import com.example.navigation_screen.databinding.ActivityMainBinding;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 
 public class Login extends AppCompatActivity{
@@ -46,88 +46,69 @@ public class Login extends AppCompatActivity{
             @Override
             public void onClick(View view) {
                 login();
-                //Toast.makeText(Login.this, "Login Successful!", Toast.LENGTH_SHORT).show();
 
-                //Toast.makeText(Login.this, "Login Failed. Try again.!", Toast.LENGTH_SHORT).show();
-                //if(outcome == "True") {
-//                    binding = ActivityMainBinding.inflate(getLayoutInflater());
-//                    setContentView(binding.getRoot());
-//
-//                    BottomNavigationView navView = findViewById(R.id.nav_view);
-//                    // Passing each menu ID as a set of Ids because each
-//                    // menu should be considered as top level destinations.
-//                    AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-//                            R.id.courses, R.id.chat, R.id.profile, R.id.schedule)
-//                            .build();
-//                    NavController navController = Navigation.findNavController(Login.this, R.id.nav_host_fragment_activity_main);
-//                    NavigationUI.setupActionBarWithNavController(Login.this, navController, appBarConfiguration);
-//                    NavigationUI.setupWithNavController(binding.navView, navController);
-                //}
+                binding = ActivityMainBinding.inflate(getLayoutInflater());
+                setContentView(binding.getRoot());
+
+                BottomNavigationView navView = findViewById(R.id.nav_view);
+                // Passing each menu ID as a set of Ids because each
+                // menu should be considered as top level destinations.
+                AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+                        R.id.courses, R.id.chat, R.id.profile, R.id.schedule)
+                        .build();
+                NavController navController = Navigation.findNavController(Login.this, R.id.nav_host_fragment_activity_main);
+                NavigationUI.setupActionBarWithNavController(Login.this, navController, appBarConfiguration);
+                NavigationUI.setupWithNavController(binding.navView, navController);
 
             }
         });
     }
     private void login() {
+        // URL endpoint for adding courses
+        String url = "http://coms-309-030.class.las.iastate.edu:8080/login";
 
         String username = login_username.getText().toString();
         String password = login_password.getText().toString();
 
-        // URL endpoint for adding courses
-        String url = "http://coms-309-030.class.las.iastate.edu:8080/users/" + username;
-
+        // JSON object that will contain the payload of the POST request
+        JSONObject postData = new JSONObject();
+        try {
+            postData.put("username", username);
+            postData.put("password", password);
+        } catch (JSONException e) {
+            // Print stack trace for any JSON exception while populating postData
+            e.printStackTrace();
+        }
 
         RequestQueue requestQueue = Volley.newRequestQueue(this);
 
-        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(Request.Method.GET, url, null,
-                // Listener for successful responses
-                new Response.Listener<JSONArray>() {
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(
+                Request.Method.POST,
+                url,
+                postData,
+                new Response.Listener<JSONObject>() {
                     @Override
-                    public void onResponse(JSONArray response) {
-                            String jsonResponse = response.toString();
-                            System.out.println(jsonResponse);
+                    public void onResponse(JSONObject response) {
+                        String jsonResponse = response.toString();
+                        System.out.println(jsonResponse);
+                        Log.d("DEBUG", jsonResponse);
 
-                            if(jsonResponse != null && jsonResponse.length() > 1){
-                                handleLoginSuccessful();
-                            }else{
-                                handleLoginFail();
-                            }
+                        if(jsonResponse == null){
+
+                        }
                     }
                 },
-                // Listener for error responses
                 new Response.ErrorListener() {
                     @Override
                     public void onErrorResponse(VolleyError error) {
-                        // Log error responses with tag "VolleyError"
-                        Log.e("VolleyError", error.toString());
+                        // handle error here
                     }
-                });
-//
+                }
+        );
 
-        requestQueue.add(jsonArrayRequest);
+        requestQueue.add(jsonObjectRequest);
         //Volley.newRequestQueue(getContext()).add(jsonObjectRequest);
 
-    }
-
-    //Login is successful! Navigate to next screen.
-    private void handleLoginSuccessful(){
-        Toast.makeText(Login.this, "Login Successful!", Toast.LENGTH_SHORT).show();
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
-
-        BottomNavigationView navView = findViewById(R.id.nav_view);
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.courses, R.id.chat, R.id.profile, R.id.schedule)
-                .build();
-        NavController navController = Navigation.findNavController(Login.this, R.id.nav_host_fragment_activity_main);
-        NavigationUI.setupActionBarWithNavController(Login.this, navController, appBarConfiguration);
-        NavigationUI.setupWithNavController(binding.navView, navController);
-    }
-
-    //Login is invalid. Doesn't move screen forward.
-    private void handleLoginFail(){
-        Toast.makeText(Login.this, "Login Failed. Please try again.", Toast.LENGTH_SHORT).show();
     }
 
 }
