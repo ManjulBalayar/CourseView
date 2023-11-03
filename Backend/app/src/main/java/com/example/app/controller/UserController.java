4aa3d0d015ae9a6ebca80524b08a4ef411993705
Manjul Balayar <mbalayar@iastate.edu>
2023-11-03 10:54:39 -0400

    studentID ---> userID

--- Backend/app/src/main/java/com/example/app/controller/UserController.java
+++ Backend/app/src/main/java/com/example/app/controller/UserController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
+import java.util.Set;
 
 @RestController
 public class UserController {
@@ -29,6 +30,38 @@ public class UserController {
     @GetMapping({"users/all"})
     List<UserProfile> GetAllUsers() {
         return this.userRepository.findAll();
+    }
+
+    @GetMapping("/userprofiles")
+    public List<UserProfile> getAllUserProfiles() {
+        return userRepository.findAll();
+    }
+
+    @GetMapping("/userprofile/{userid}")
+    public Optional<UserProfile> getUserProfile(@PathVariable("userid") Long userid) {
+        return userRepository.findById(userid);
+    }
+
+    // Get certain user's course list
+    @GetMapping("/userprofile/{userid}/courses")
+    public Set<Course> getCoursesLikedByUserProfile(@PathVariable("userid") Long userid) {
+
+        UserProfile userProfile = userRepository.findById(userid)
+                .orElseThrow(() -> new RuntimeException("User not found"));
+
+        return userProfile.getCourses();
+    }
+
+    // Gets all user names only
+    @GetMapping("/userprofiles/names")
+    public List<Map<String, Object>> getNames() {
+        return userRepository.findAllUserProfileNames();
+    }
+
+    // Gets all users that start with "search"
+    @GetMapping("/userprofiles/{search}")
+    public List<UserProfile> getSearched(@PathVariable("search") String search) {
+        return userRepository.findByNameStartsWithSorted(search);
     }
 
     @PostMapping({"users/post"})
