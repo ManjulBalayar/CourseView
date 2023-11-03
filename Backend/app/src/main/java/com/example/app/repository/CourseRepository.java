2e4e39094bd1149c8e7c99eca302216f3ee3c9b4
Manjul Balayar <mbalayar@iastate.edu>
2023-11-03 16:18:21 -0400

    Removed courses from UserProfile

--- Backend/app/src/main/java/com/example/app/repository/CourseRepository.java
+++ Backend/app/src/main/java/com/example/app/repository/CourseRepository.java
@@ -7,6 +7,4 @@ import org.springframework.data.jpa.repository.Query;
 import java.util.List;
 
 public interface CourseRepository extends JpaRepository<Course, Long> {
-    @Query("FROM Course WHERE name LIKE ?1% ORDER BY name")
-    List<Course> findByNameStartsWithSorted(String courseName);
 }
