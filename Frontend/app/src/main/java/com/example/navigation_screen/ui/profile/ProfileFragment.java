bba316071f3fb6adb6eedaaff04e92e15773f5b7
dalamsya50 <devin.alamsya@gmail.com>
2023-11-04 00:12:48 -0500

    View course ID's of the courses rated by user

--- Frontend/app/src/main/java/com/example/navigation_screen/ui/profile/ProfileFragment.java
+++ Frontend/app/src/main/java/com/example/navigation_screen/ui/profile/ProfileFragment.java
@@ -1,5 +1,6 @@
 package com.example.navigation_screen.ui.profile;
 
+import android.app.AlertDialog;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -26,6 +27,9 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ProfileFragment extends Fragment {
 
     private FragmentProfileBinding binding;
@@ -33,6 +37,11 @@ public class ProfileFragment extends Fragment {
     TextView textUsername, textEmail;
 
     Button viewReviewsBtn;
+
+    private List<String> courseIds = new ArrayList<>();
+    private List<String> courseDescriptions = new ArrayList<>();
+
+    private List<String> courseNames = new ArrayList<>();
 
     int userid = Login.userid;
 
@@ -117,14 +126,69 @@ public class ProfileFragment extends Fragment {
                     @Override
                     public void onResponse(JSONArray response) {
                         System.out.println(response.toString());
+                        try {
+
+                            courseIds.clear();
+
+                            // Iterate through the JSON array response
+                            for (int i = 0; i < response.length(); i++) {
+                                // Get each course as a JSON object
+                                JSONObject course = response.getJSONObject(i);
+                                // Extract and store relevant information from each course object
+                                String id = course.getString("courseId");
+                                System.out.println(id);
+                                convertToName(id);
+                                courseIds.add(id);
+                            }
+
+                            System.out.println(response.toString());
+                            AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+                            builder.setTitle("Courses Reviewed:");
+                            builder.setMessage(courseIds.toString());
+                            builder.setPositiveButton("OK", null);
+                            AlertDialog dialog = builder.create();
+                            dialog.show();
+                        } catch (JSONException e) {
+                            // Print stack trace for JSONException
+                            e.printStackTrace();
+                        }
+                    }
+                },
+                // Listener for error responses
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        // Log error responses with tag "VolleyError"
+                        Log.e("VolleyError", error.toString());
+                    }
+                });
+
+        // Add the created request to the Volley request queue
+        Volley.newRequestQueue(getContext()).add(jsonArrayRequest);
+    }
+
+    private void convertToName(String courseId) {
+        // URL pointing to the courses resource
+        String url = "http://coms-309-030.class.las.iastate.edu:8080/courses/" + courseId;
+
+        // Create a new JSON array request to receive a JSON array from the given URL
+        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(Request.Method.GET, url, null,
+                // Listener for successful responses
+                new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        System.out.println(response.toString());
 //                        try {
-//                            System.out.println(response.toString());
-//                            AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
-//                            builder.setTitle("Courses Reviewed");
-//                            builder.setMessage("Your calculated workload is: ");
-//                            builder.setPositiveButton("OK", null);
-//                            AlertDialog dialog = builder.create();
-//                            dialog.show();
+//                            // Get each course as a JSON object
+//                            // Extract and store relevant information from each course object
+//                            JSONObject course = response.getJSONObject(0);
+//                            String id = response.getString("courseid");
+//                            String name = response.getString("name");
+//                            String description = response.getString("description");
+//                            System.out.println(name);
+//                            System.out.println(description);
+//                            System.out.println(id);
+//
 //                        } catch (JSONException e) {
 //                            // Print stack trace for JSONException
 //                            e.printStackTrace();
