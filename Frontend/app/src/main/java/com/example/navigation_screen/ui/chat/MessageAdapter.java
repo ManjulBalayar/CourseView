cbac4af7d7d14b997acafa64a43d22c57f24c01c
BenjaminMuslic <muslic.ben@gmail.com>
2023-11-12 02:42:43 -0600

    more commits

--- Frontend/app/src/main/java/com/example/navigation_screen/ui/chat/MessageAdapter.java
+++ Frontend/app/src/main/java/com/example/navigation_screen/ui/chat/MessageAdapter.java
@@ -11,20 +11,39 @@ import com.example.navigation_screen.R;
 
 import java.util.List;
 
+/**
+ * Adapter for the RecyclerView in a chat interface.
+ * This adapter manages message views depending on whether they are sent or received.
+ */
 public class MessageAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
 
+    // List of messages to be displayed in the RecyclerView.
     private List<Message> messages;
 
+    // View type constants for sent and received messages.
     private static final int VIEW_TYPE_SENT = 0;
-
     private static final int VIEW_TYPE_IGNORE = 2;
-
     private static final int VIEW_TYPE_RECEIVED = 1;
 
+    /**
+     * Constructor for MessageAdapter.
+     *
+     * @param messages List of Message objects to be displayed in the RecyclerView
+     */
     public MessageAdapter(List<Message> messages) {
         this.messages = messages;
     }
 
+    /**
+     *
+     * Called when RecyclerView needs a new ViewHolder of the given type to represent an item.
+     *
+     * @param parent The ViewGroup into which the new View will be added after it is bound to
+     *               an adapter position.
+     * @param viewType The view type of the new View.
+     *
+     * @return
+     */
     @Override
     public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         View view;
@@ -39,6 +58,13 @@ public class MessageAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder
         }
     }
 
+    /**
+     * Called by RecyclerView to display the data at the specified position.
+     *
+     * @param holder The ViewHolder which should be updated to represent the contents of the
+     *        item at the given position in the data set.
+     * @param position The position of the item within the adapter's data set.
+     */
     @Override
     public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
         Message message = messages.get(position);
@@ -50,7 +76,12 @@ public class MessageAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder
         // Do nothing for invalid type (-1)
     }
 
-
+    /**
+     * Returns the view type of the item at position for the purposes of view recycling.
+     *
+     * @param position position to query
+     * @return The view type (sent or received) of the item at position.
+     */
     @Override
     public int getItemViewType(int position) {
         Message message = messages.get(position);
@@ -63,7 +94,11 @@ public class MessageAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder
         }
     }
 
-
+    /**
+     * Returns the total number of items in the data set held by the adapter.
+     *
+     * @return The total number of items in this adapter.
+     */
     @Override
     public int getItemCount() {
         int count = 0;
@@ -76,8 +111,12 @@ public class MessageAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder
     }
 
 
+    /**
+     * ViewHolder for sent messages.
+     */
     private class SentMessageHolder extends RecyclerView.ViewHolder {
         TextView messageText;
+
 
         SentMessageHolder(View itemView) {
             super(itemView);
@@ -89,6 +128,9 @@ public class MessageAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder
         }
     }
 
+    /**
+     * ViewHolder for received messages.
+     */
     private class ReceivedMessageHolder extends RecyclerView.ViewHolder {
         TextView messageText;
 
