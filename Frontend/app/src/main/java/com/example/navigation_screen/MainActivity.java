310c1076b5b8da64053603c6ae51945c1aa24819
Benjamin Muslic <muslicb@iastate.edu>
2023-11-23 20:16:43 -0600

    sends periodic notification to users device

--- Frontend/app/src/main/java/com/example/navigation_screen/MainActivity.java
+++ Frontend/app/src/main/java/com/example/navigation_screen/MainActivity.java
@@ -1,13 +1,21 @@
 package com.example.navigation_screen;
 
+import android.annotation.SuppressLint;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 
 import com.bumptech.glide.Glide;
 import com.daimajia.androidanimations.library.Techniques;
@@ -22,6 +30,15 @@ public class MainActivity extends AppCompatActivity {
     // Buttons for the user to choose between login and signup.
     Button loginchoice, signupchoice;
 
+    private final Handler handler = new Handler(Looper.getMainLooper());
+    private final Runnable notificationRunnable = new Runnable() {
+        @Override
+        public void run() {
+            sendRandomNotification();
+            handler.postDelayed(this, 5000); // Schedule again in 10 seconds
+        }
+    };
+
     /**
      * Called when the activity is starting.
      * Initializes the activity, sets the content view, and sets up the UI components.
@@ -29,6 +46,7 @@ public class MainActivity extends AppCompatActivity {
      *
      * @param savedInstanceState
      */
+    @SuppressLint("MissingPermission")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -38,6 +56,10 @@ public class MainActivity extends AppCompatActivity {
 
         loginchoice = findViewById(R.id.loginchoice);
         signupchoice = findViewById(R.id.signupchoice);
+
+        createNotificationChannel();
+        handler.post(notificationRunnable); // Start the periodic notifications
+
 
         //Welcome to CourseView! animation
         TextView textView = findViewById(R.id.choice);
@@ -70,18 +92,10 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-//        private void createNotificationChannel() {
-//            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-//                CharSequence name = getString(R.string.channel_name);
-//                String description = getString(R.string.channel_description);
-//                int importance = NotificationManager.IMPORTANCE_DEFAULT;
-//                NotificationChannel channel = new NotificationChannel("CHANNEL_ID", name, importance);
-//                channel.setDescription(description);
-//                // Register the channel with the system
-//                NotificationManager notificationManager = getSystemService(NotificationManager.class);
-//                notificationManager.createNotificationChannel(channel);
-//            }
-//        }
+
+
+
+
 
 
 
@@ -100,6 +114,38 @@ public class MainActivity extends AppCompatActivity {
         NavigationUI.setupWithNavController(binding.navView, navController);
         */
     }
+    private void createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = getString(R.string.channel_name);
+            String description = getString(R.string.channel_description);
+            int importance = NotificationManager.IMPORTANCE_HIGH;
+            NotificationChannel channel = new NotificationChannel("CHANNEL_ID", name, importance);
+            channel.setDescription(description);
+            // Register the channel with the system
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
+    }
+
+    @SuppressLint("MissingPermission")
+    private void sendRandomNotification() {
+        String message = "Save yourself a headache next semester and check your schedule!";
+
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, "CHANNEL_ID")
+                .setSmallIcon(R.drawable.logo1)
+                .setContentTitle("Registered?")
+                .setContentText(message)
+                .setPriority(NotificationCompat.PRIORITY_HIGH);
+
+        builder.setVisibility(NotificationCompat.VISIBILITY_PUBLIC);
+
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
+        notificationManager.notify(1, builder.build());
+
+    }
+
+
 
 
 }
--- Frontend/app/src/main/java/com/example/navigation_screen/ReminderBroadcast.java
+++
@@ -1,6 +0,0 @@
-//package com.example.navigation_screen;
-//
-//import android.content.BroadcastReceiver;
-//
-//public class ReminderBroadcast extends BroadcastReceiver {
-//}
