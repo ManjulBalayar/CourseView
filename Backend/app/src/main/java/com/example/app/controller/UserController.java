38e2844f5d80ecc1d9db0a421da3d3d387d316a0
Manjul Balayar <mbalayar@iastate.edu>
2023-12-06 01:06:12 -0600

    Delete and Update Test

--- Backend/app/src/main/java/com/example/app/controller/UserController.java
+++ Backend/app/src/main/java/com/example/app/controller/UserController.java
@@ -30,94 +30,49 @@ public class UserController {
     @Autowired
     ScheduleRepository scheduleRepository;
 
-//    @Autowired
-//    private CourseRepository courseRepository;
+    @Autowired
+    private CourseRepository courseRepository;
 
-    /**
-     * Retrieves a UserProfile entity by its username.
-     *
-     * @param username the username of the UserProfile to retrieve
-     * @return the UserProfile entity with the specified username
-     */
     @GetMapping({"users/searchByName/{username}"})
     public UserProfile getUserByName(@PathVariable("username") String username) {
         return userRepository.findByUsername(username);
     }
 
-    /**
-     * Retrieves a UserProfile entity by its ID.
-     *
-     * @param user_id the ID of the UserProfile to retrieve
-     * @return an Optional containing the UserProfile entity if found, or an empty Optional if not found
-     */
     @GetMapping("/users/{user_id}")
     public Optional<UserProfile> getStudent(@PathVariable("user_id") Long user_id) {
         return userRepository.findById(user_id);
     }
 
-    /**
-     * Retrieves all UserProfile entities in the database.
-     *
-     * @return a list of all UserProfile entities
-     */
     @GetMapping({"users/all"})
     List<UserProfile> GetAllUsers() {
         return this.userRepository.findAll();
     }
 
-    /**
-     * Retrieves all UserProfile entities in the database.
-     *
-     * @return a list of all UserProfile entities
-     */
     @GetMapping("/userprofiles")
     public List<UserProfile> getAllUserProfiles() {
         return userRepository.findAll();
     }
 
-    /**
-     * Retrieves a UserProfile entity by its user ID.
-     *
-     * @param userid the user ID of the UserProfile to retrieve
-     * @return an Optional containing the UserProfile entity if found, or an empty Optional if not found
-     */
     @GetMapping("/userprofile/{userid}")
     public Optional<UserProfile> getUserProfile(@PathVariable("userid") Long userid) {
         return userRepository.findById(userid);
     }
 
-    /**
-     * Retrieves a list of usernames and their corresponding IDs.
-     *
-     * @return a list of UsernameAndId objects containing usernames and their IDs
-     */
     @GetMapping("/userprofiles/names")
     public List<UsernameAndId> getNames() {
         return userRepository.findAllByOrderByUsername();
     }
 
-    /**
-     * Retrieves a list of UserProfile entities that start with a specified search string.
-     *
-     * @param search the search string to filter usernames
-     * @return a list of UserProfile entities matching the search criteria
-     */
     @GetMapping("/userprofiles/{search}")
     public List<UserProfile> getSearched(@PathVariable("search") String search) {
         return userRepository.findByUsernameStartingWithOrderByUsername(search);
     }
 
-    @GetMapping({"/byRoles/{role}"})
-    public List<UserProfile> getUserByRole(@PathVariable("role") String role) {
-        return userRepository.findByRole(role);
-    }
+//    @GetMapping({"/byRoles/{role}"})
+//    public List<UserProfile> getUserByRole(@PathVariable("role") String role) {
+//        return userRepository.findByRole(role);
+//    }
 
-    /**
-     * Adds a new UserProfile entity to the database and creates an associated empty Schedule entity.
-     *
-     * @param newUser the new UserProfile entity to be added
-     * @return the added UserProfile entity
-     */
     @PostMapping({"users/post"})
     UserProfile PostUserByBody(@RequestBody UserProfile newUser) {
         this.userRepository.save(newUser);
@@ -128,43 +83,43 @@ public class UserController {
     }
 
 
-//    @DeleteMapping("/courses/delete/{courseId}/{username}")
-//    public ResponseEntity<?> deleteCourse(@PathVariable Long courseId, @PathVariable String username) {
-//        UserProfile user = userRepository.findByUsername(username);
-//        if (user != null && "Admin".equals(user.getRole())) {
-//            if (courseRepository.existsById(courseId)) {
-//                courseRepository.deleteById(courseId);
-//                return ResponseEntity.ok().build();
-//            } else {
-//                return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Course not found");
-//            }
-//        }
-//        return ResponseEntity.status(HttpStatus.FORBIDDEN).body("Unauthorized to delete course");
-//    }
+    @DeleteMapping("/courses/delete/{courseId}/{username}")
+    public ResponseEntity<?> deleteCourse(@PathVariable Long courseId, @PathVariable String username) {
+        UserProfile user = userRepository.findByUsername(username);
+        if (user != null && "Admin".equals(user.getRole())) {
+            if (courseRepository.existsById(courseId)) {
+                courseRepository.deleteById(courseId);
+                return ResponseEntity.ok().build();
+            } else {
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Course not found");
+            }
+        }
+        return ResponseEntity.status(HttpStatus.FORBIDDEN).body("Unauthorized to delete course");
+    }
 
-//    @PutMapping("/courses/update/{courseId}/{userId}")
-//    public ResponseEntity<?> updateCourse(@PathVariable Long courseId, @PathVariable Long userId, @RequestBody Course updatedCourse) {
-//        Optional<UserProfile> userOptional = userRepository.findById(userId);
-//
-//        if (userOptional.isPresent()) {
-//            UserProfile user = userOptional.get();
-//            if ("Admin".equals(user.getRole())) {
-//                Optional<Course> courseOptional = courseRepository.findById(courseId);
-//                if (courseOptional.isPresent()) {
-//                    Course course = courseOptional.get();
-//                    course.setName(updatedCourse.getName());
-//                    course.setDescription(updatedCourse.getDescription());
-//                    course.setDepartment(updatedCourse.getDepartment());
-//                    courseRepository.save(course);
-//
-//                    return ResponseEntity.ok().body("Course updated successfully");
-//                } else {
-//                    return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Course not found");
-//                }
-//            }
-//        }
-//        return ResponseEntity.status(HttpStatus.FORBIDDEN).body("Unauthorized to update course");
-//    }
+    @PutMapping("/courses/update/{courseId}/{userId}")
+    public ResponseEntity<?> updateCourse(@PathVariable Long courseId, @PathVariable Long userId, @RequestBody Course updatedCourse) {
+        Optional<UserProfile> userOptional = userRepository.findById(userId);
+
+        if (userOptional.isPresent()) {
+            UserProfile user = userOptional.get();
+            if ("Admin".equals(user.getRole())) {
+                Optional<Course> courseOptional = courseRepository.findById(courseId);
+                if (courseOptional.isPresent()) {
+                    Course course = courseOptional.get();
+                    course.setName(updatedCourse.getName());
+                    course.setDescription(updatedCourse.getDescription());
+                    course.setDepartment(updatedCourse.getDepartment());
+                    courseRepository.save(course);
+
+                    return ResponseEntity.ok().body("Course updated successfully");
+                } else {
+                    return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Course not found");
+                }
+            }
+        }
+        return ResponseEntity.status(HttpStatus.FORBIDDEN).body("Unauthorized to update course");
+    }
 
 
 }
--- Backend/app/src/test/java/com/example/app/UserControllerTest.java
+++ Backend/app/src/test/java/com/example/app/UserControllerTest.java
@@ -5,6 +5,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 import static org.hamcrest.Matchers.*;
 
+import com.example.app.model.Course;
 import com.example.app.model.Schedule;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
@@ -21,6 +22,7 @@ import com.example.app.controller.UserController;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 @WebMvcTest(UserController.class)
 public class UserControllerTest {
@@ -33,6 +35,9 @@ public class UserControllerTest {
 
     @MockBean
     private ScheduleRepository scheduleRepository;
+
+    @MockBean
+    private CourseRepository courseRepository;
 
     @Test
     public void getAllUserProfilesTest() throws Exception {
@@ -67,5 +72,77 @@ public class UserControllerTest {
 
         verify(userRepository, times(1)).save(any(UserProfile.class));
         verify(scheduleRepository, times(1)).save(any(Schedule.class));
+    }
+
+    @Test
+    public void adminDeletesCourseSuccessfully() throws Exception {
+        Long courseId = 1L;
+        String adminUsername = "adminUser";
+        UserProfile adminUser = new UserProfile();
+        adminUser.setUsername(adminUsername);
+        adminUser.setRole("Admin");
+
+        when(userRepository.findByUsername(adminUsername)).thenReturn(adminUser);
+        when(courseRepository.existsById(courseId)).thenReturn(true);
+
+        mockMvc.perform(delete("/courses/delete/{courseId}/{username}", courseId, adminUsername))
+                .andExpect(status().isOk());
+
+        verify(courseRepository, times(1)).deleteById(courseId);
+    }
+
+    @Test
+    public void nonAdminUserAttemptsToDeleteCourse() throws Exception {
+        Long courseId = 1L;
+        String nonAdminUsername = "user";
+        UserProfile nonAdminUser = new UserProfile();
+        nonAdminUser.setUsername(nonAdminUsername);
+        nonAdminUser.setRole("User");
+
+        when(userRepository.findByUsername(nonAdminUsername)).thenReturn(nonAdminUser);
+
+        mockMvc.perform(delete("/courses/delete/{courseId}/{username}", courseId, nonAdminUsername))
+                .andExpect(status().isForbidden())
+                .andExpect(content().string("Unauthorized to delete course"));
+    }
+
+    @Test
+    public void adminUserDeletesNonExistingCourse() throws Exception {
+        Long courseId = 1L;
+        String adminUsername = "adminUser";
+        UserProfile adminUser = new UserProfile();
+        adminUser.setUsername(adminUsername);
+        adminUser.setRole("Admin");
+
+        when(userRepository.findByUsername(adminUsername)).thenReturn(adminUser);
+        when(courseRepository.existsById(courseId)).thenReturn(false);
+
+        mockMvc.perform(delete("/courses/delete/{courseId}/{username}", courseId, adminUsername))
+                .andExpect(status().isNotFound())
+                .andExpect(content().string("Course not found"));
+    }
+
+    @Test
+    public void adminUpdatesCourseSuccessfully() throws Exception {
+        Long courseId = 1L;
+        Long adminUserId = 1L;
+        UserProfile adminUser = new UserProfile();
+        adminUser.setUser_id(adminUserId);
+        adminUser.setRole("Admin");
+        Course updatedCourse = new Course();
+        updatedCourse.setName("Updated Course Name");
+        updatedCourse.setDescription("Updated Description");
+        // Set other updated fields as needed
+
+        when(userRepository.findById(adminUserId)).thenReturn(Optional.of(adminUser));
+        when(courseRepository.findById(courseId)).thenReturn(Optional.of(new Course()));
+
+        String jsonRequestBody = new ObjectMapper().writeValueAsString(updatedCourse);
+
+        mockMvc.perform(put("/courses/update/{courseId}/{userId}", courseId, adminUserId)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(jsonRequestBody))
+                .andExpect(status().isOk())
+                .andExpect(content().string("Course updated successfully"));
     }
 }
